f3f5c8503ece3986c04d4f4de8257c71dc989c2d
鸣上 <mingshang.tao@tuya.com>
2024-07-26 17:52:45 +0800

    refresh apikey

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,18 @@
 import { defineBackend } from '@aws-amplify/backend';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { refreshApiKey } from './jobs/refreshApiKey/resource';
 
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
+  refreshApiKey,
 });
+
+backend.auth.resources.unauthenticatedUserIamRole.addToPrincipalPolicy(
+  new PolicyStatement({
+    actions: ['appsync:UpdateApiKey'],
+    resources: ['*'],
+  }),
+);
