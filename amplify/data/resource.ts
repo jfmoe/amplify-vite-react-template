65bd8e9b0e7a7fd92ebfaec28ab9150489322f34
luolin.tong@tuya.com <luolin.tong@tuya.com>
2024-07-30 19:20:10 +0800

    user

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,6 +7,17 @@ specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
+  Todo: a
+  .model({
+    content: a.string(),
+    devId: a.string().default('')
+  })
+  .authorization((allow) => [allow.publicApiKey().to(["create", "update", "get", "list", "search", "listen", "sync"])]),
+  Logs: a
+  .model({
+    content: a.string(),
+  })
+  .authorization((allow) => [allow.publicApiKey().to(["create", "update", "get", "list", "search", "listen", "sync"])]),
   User: a
     .model({
       ownerId: a.string().default(''),
