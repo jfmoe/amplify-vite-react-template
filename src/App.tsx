50eb2553aca74817e78eea42e7496af2ed7e4abd
mingshang.tao <mingshang.tao@tuya.com>
2024-11-29 15:26:17 +0800

    feat: ttl

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,45 @@
-import React, { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-import '@aws-amplify/ui-react/styles.css'
-
+import React, { useEffect, useState } from 'react';
+import type { Schema } from '../amplify/data/resource';
+import { generateClient } from 'aws-amplify/data';
+import '@aws-amplify/ui-react/styles.css';
 
 const client = generateClient<Schema>();
 const App: React.FC = () => {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-  const [logs, setLogs] = useState<Array<Schema["Logs"]["type"]>>([]);
+  const [todos, setTodos] = useState<Array<Schema['Todo']['type']>>([]);
+  const [logs, setLogs] = useState<Array<Schema['Logs']['type']>>([]);
 
   const fetchData = () => {
     try {
       client.models.Todo.observeQuery().subscribe({
-        next: (data) => setTodos([...data.items]),
+        next: data => setTodos([...data.items]),
       });
-  
+
       client.models.Logs.observeQuery().subscribe({
-        next: (data) => setLogs([...data.items]),
+        next: data => setLogs([...data.items]),
       });
     } catch (e) {
       console.warn(e);
-     }
-  }
+    }
+  };
 
   useEffect(() => {
     fetchData();
   }, []);
 
   function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content"), devId: "123" });
+    client.models.Todo.create({
+      content: window.prompt('Todo content'),
+      devId: '123',
+      _ttl: Date.now() / 1000 + 60,
+    });
   }
 
   function deleteToDo(id: string) {
     client.models.Todo.delete({ id });
+  }
+
+  function updateToDo(id: string) {
+    client.models.Todo.update({ id, content: window.prompt('Todo content') });
   }
 
   function deleteLog(id: string) {
@@ -40,7 +47,7 @@ const App: React.FC = () => {
   }
 
   function createLogs() {
-    client.models.Logs.create({ content: window.prompt("Logs content") });
+    client.models.Logs.create({ content: window.prompt('Logs content') });
   }
 
   return (
@@ -48,19 +55,23 @@ const App: React.FC = () => {
       <h1>User Auth My Logs</h1>
       <button onClick={createLogs}>+ new log</button>
       <ul>
-        {logs.map((log) => (
-          <li key={log.id} onClick={() => deleteLog(log.id)}>{log.content}</li>
+        {logs.map(log => (
+          <li key={log.id} onClick={() => deleteLog(log.id)}>
+            {log.content}
+          </li>
         ))}
       </ul>
       <h1>Public Auth My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
-        {todos.map((todo) => (
-          <li key={todo.id} onClick={() => deleteToDo(todo.id)}>{todo.content}</li>
+        {todos.map(todo => (
+          <li key={todo.id} onClick={() => updateToDo(todo.id)}>
+            {todo.content}
+          </li>
         ))}
       </ul>
     </main>
   );
-}
+};
 
 export default React.memo(App);
