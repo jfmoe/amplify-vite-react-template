81fc1166c48ad9a5351191eec5d3d94b7270c970
mingshang.tao <mingshang.tao@tuya.com>
2024-11-29 10:46:19 +0800

    feat: enable point in time recovery

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -10,11 +10,17 @@ const backend = defineBackend({
   refreshApiKey,
 });
 
+// 赋予 Lambda 函数更新 API Key 的权限
 const refreshApiKeyLambda = backend.refreshApiKey.resources.lambda;
-
 refreshApiKeyLambda.addToRolePolicy(
   new PolicyStatement({
     actions: ['appsync:UpdateApiKey'],
     resources: ['*'],
   }),
 );
+
+// 容灾处理：为所有 table 开启时间点恢复功能
+const { amplifyDynamoDbTables } = backend.data.resources.cfnResources;
+for (const table of Object.values(amplifyDynamoDbTables)) {
+  table.pointInTimeRecoveryEnabled = true;
+}
